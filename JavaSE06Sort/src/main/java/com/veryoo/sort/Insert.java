2d3473d0bc5027e193bab077960f72f9569cf8f3
oubijie <oubijie@139.com>
2019-08-05 23:45:14 +0800

    排序补充

--- JavaSE06Sort/src/main/java/com/veryoo/sort/Insert.java
+++ JavaSE06Sort/src/main/java/com/veryoo/sort/Insert.java
@@ -20,8 +20,24 @@ public class Insert {
 		
 	}
 	
-	//实现方式一
+	
 	public static void insertSort(int[] arr){
+		for(int i=1; i<arr.length; i++) {
+			int temp = arr[i];
+			int j;
+			for(j=i-1; j>=0; j--) {
+				if(arr[j] > temp) {   //将大于temp的元素往后挪一位，直到找到比temp小的
+					arr[j+1] = arr[j];
+				}else {
+					break;
+				}
+			}
+			arr[j+1] = temp;
+		}
+	}
+	
+	//实现方式一
+	public static void insertSort2(int[] arr){
 		for(int i=1; i<arr.length; i++){
 			for(int j=0; j<i; j++){
 				if(arr[i] < arr[j]){
@@ -36,7 +52,8 @@ public class Insert {
 		}
 	}
 	
-	public static void insertSort2(int[] arr){
+	//实现方式二
+	public static void insertSort3(int[] arr){
 		for(int i=1; i<arr.length; i++){
 			//先找出要插入的目标位置
 			int dest = i;
--- JavaSE06Sort/src/main/java/com/veryoo/sort/Test03.java
+++ JavaSE06Sort/src/main/java/com/veryoo/sort/Test03.java
@@ -12,20 +12,30 @@ import java.util.Arrays;
 public class Test03 {
 
 	public static void main(String[] args) {
-		int[] a = {4,2,7,3,8,2};
+		int[] arr = {6,7,1,4};
+		
+		//复制数组，产生一个新的数组，可以指定新数组的长度
+		int[] newarr = Arrays.copyOf(arr, 10);
+		
+		//将数组转换成可阅读的字符串
+		System.out.println(Arrays.toString(newarr));
+		
+		//给数组填充内容
+//		Arrays.fill(arr, 9);
+		System.out.println(Arrays.toString(arr));
 		
 		//排序
-		Arrays.sort(a);
+//		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
 		
-		//打印
-		String result = Arrays.toString(a);
-		System.out.println(result);
-		
-		//复制
-		int[] b = Arrays.copyOf(a, 4);
-		System.out.println(Arrays.toString(b));
+		//二分查找，数组要先有序
+		int index = Arrays.binarySearch(arr, 9);
+		System.out.println(index);
 		
 		
-//		System.arraycopy(src, srcPos, dest, destPos, length)
+		//数组复制
+		int[] na = new int[10];
+		System.arraycopy(arr, 0, na, 0, 4);
+		System.out.println(Arrays.toString(na));
 	}
 }
